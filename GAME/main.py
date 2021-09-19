958c739f81789a74f186a405e0a5cfaf5a052e47
cat_or_not <41955154+catornot@users.noreply.github.com>
2021-09-19 15:58:31 -0400

    added a settings file

--- GAME/main.py
+++ GAME/main.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 from pygame.locals import *
 from pygame import mixer
-from random import randint
+from random import randint as rd
 
 # Initialise PyGame.
 pygame.init()
@@ -12,8 +12,25 @@ fps = 60.0
 fpsClock = pygame.time.Clock()
 
 # Set up the window.
-width, height = int(1920), int(1080)
-screen = pygame.display.set_mode((width, height))
+with open("settings.txt","r") as file:
+    file = list(file.read().split("\n"))
+
+    size_x, size_y , full = 640, 480, False
+    
+    for i in file:
+        if i.startswith("#"):
+            file.pop(file.index(i))
+        if i.startswith("full"):
+            full = bool(int(i.split(":")[1]))
+        if i.startswith("size_x"):
+            size_x = int(i.split(":")[1])
+        if i.startswith("size_y"):
+            size_y = int(i.split(":")[1])
+
+    if full:
+        screen = pygame.display.set_mode((size_x, size_y),FULLSCREEN)
+    else:
+        screen = pygame.display.set_mode((size_x, size_y))
 
 # icon
 # icon = pygame.image.load("none")
@@ -31,6 +48,9 @@ while running:
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             running = False
+
+    #testing circle
+    pygame.draw.circle(screen,(rd(90,120),255,255),(100,100),rd(10,100))
 
     pygame.display.update()
 
