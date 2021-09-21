5e61dcf422204803946489a3afb85fc3feaaf496
cat_or_not <41955154+catornot@users.noreply.github.com>
2021-09-20 20:44:32 -0400

    adding partially wall and images

--- GAME/code/wall.py
+++ GAME/code/wall.py
@@ -1,7 +1,16 @@
 import pygame
 from pygame.locals import *
 
-class wall():
-    class MyWindow(pygame.Surface):
-        def __init__(self, Adujuster):
-            pygame.Surface.__init__(self, size=(w, h))
+class wall_manager():
+    def __init__(self,img):
+        #the image is the type of the wall.
+        self.img = img
+        self.walls = []
+
+    def add_wall(self,x,y,size):
+        pass
+
+class wall(pygame.Surface):
+    def __init__(self ,w , h, img):
+        pygame.Surface.__init__(self, size=(w, h))
+        self.blit(img, (0 ,0)
